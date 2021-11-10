c7993cce0d6031fa358e70dfa9eed1e6f4fad847
Lutsia <nazarova_lu@psu.ru>
2021-11-10 19:01:43 +0500

    commit

--- js/JSON.js
+++ js/JSON.js
@@ -7,6 +7,26 @@ fetch("https://randomuser.me/api/")
             console.log(error)
         }) 
 
-     
+        
+async function getResponse(){
+    let response = await fetch ('https://randomuser.me/api/')
+    let content = await response.json()
+    content = content["results"][0]
 
+    let list = document.querySelector('.name')
+    let key;
 
+    for (key in content){
+        console.log(key)
+        list.innerHTML+=` 
+            <li class="post">
+                <h4>${key}</h4> 
+                <img src="${content[key].large}}" width="300"></img>
+                
+            </li>
+            `
+    }
+
+}
+
+getResponse()
